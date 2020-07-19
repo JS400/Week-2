be1ffc6e400bd647faf1a2822a288a26411f8f7f
Ryan Fields <rcfields@u.washington.edu>
2020-07-19 13:27:01 -0700

    finished routes for calendar

--- daos/calendars.js
+++ daos/calendars.js
@@ -3,8 +3,22 @@ const Calendars = require('../models/calendars');
 module.exports = {};
   
 module.exports.create = async (name) => {
+  try {
   return await Calendars.create({ name });
+} catch (e) {
+  return null;
+}
 };
+
+module.exports.getAll = async () => {
+  try {
+    const calendars = await Calendars.find({}).lean();
+    return calendars;
+  } catch (e) {
+    return null;
+  }
+};
+
 
 module.exports.getById = async (id) => {
   try {
@@ -14,3 +28,19 @@ module.exports.getById = async (id) => {
     return null;
   }
 };
+
+module.exports.updateById = async (calendarId, calendar) => {
+  try {
+    return await Calendars.update({ _id: calendarId }, calendar);
+  } catch (e) {
+    return null;
+  }
+};
+
+module.exports.deleteById = async (calendarId) => {
+  try {
+    await Calendars.remove({ _id: calendarId });
+  } catch (e) {
+    return null;
+  }
+}
--- routes/calendars.js
+++ routes/calendars.js
@@ -13,6 +13,15 @@ router.post("/", async (req, res, next) => {
   }
 });
 
+router.get("/", async (req, res, next) => {
+  const calendars = await CalendarDAO.getAll();
+  if (calendars) {
+    res.json(calendars);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
 router.get("/:id", async (req, res, next) => {
   const calendar = await CalendarDAO.getById(req.params.id);
   if (calendar) {
@@ -20,6 +29,28 @@ router.get("/:id", async (req, res, next) => {
   } else {
     res.sendStatus(404);
   }
+});
+
+router.put("/:id", async (req, res, next) => {
+  const calendarId = req.params.id;
+  const calendar = req.body;
+
+  if (!calendar || JSON.stringify(calendar) === "{}") {
+    res.status(400).send('Calendar is required"');
+  } else {
+    const updatedCalendar =  await CalendarDAO.updateById(calendarId, calendar);
+    res.json(updatedCalendar);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  const calendarId = req.params.id;
+  try {
+    await CalendarDAO.deleteById(calendarId);
+    res.sendStatus(200);
+    } catch(e) {
+      res.status(500).send(e.message);
+    }
 });
 
 module.exports = router;
